6cf3c91ea5f8ddd06973537f6681d01691188e11
dedmaksim03 <sergeevih.08@mail.ru>
2025-04-03 13:30:27 +0300

    Дополнительно сделан экран "Помощь"

--- app/src/main/java/com/example/progmobiledevices/MainSettingsHelp.kt
+++ app/src/main/java/com/example/progmobiledevices/MainSettingsHelp.kt
@@ -1,14 +1,10 @@
 package com.example.progmobiledevices
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.constraintlayout.widget.ConstraintSet.Layout
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [MainSettingsFragment.newInstance] factory method to
+ * Use the [MainSettingsHelp.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MainSettingsFragment : Fragment() {
+class MainSettingsHelp : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -37,23 +33,8 @@ class MainSettingsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_main_settings, container, false)
-        val profile: ConstraintLayout = view.findViewById(R.id.profile_layout)
-        val help: ConstraintLayout = view.findViewById(R.id.help)
-        profile.setOnClickListener {
-            loadFragment(MainSettingsProfile())
-        }
-        help.setOnClickListener {
-            loadFragment(MainSettingsHelp())
-        }
-        return view
-    }
-
-    private fun loadFragment(fragment: Fragment) {
-        requireActivity().supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, fragment)
-            .addToBackStack(null) // Добавляем в BackStack, чтобы можно было вернуться назад
-            .commit()
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main_settings_help, container, false)
     }
 
     companion object {
@@ -63,12 +44,12 @@ class MainSettingsFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MainSettingsFragment.
+         * @return A new instance of fragment MainSettingsHelp.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MainSettingsFragment().apply {
+            MainSettingsHelp().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
